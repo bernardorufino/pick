3f5f2d6b437e0929bcbf63f87aa24ae7f1e8f1a6
Felipe <felipe2855@gmail.com>
2015-09-03 11:16:54 -0300

    Changed color of the instructions to green

--- pick.py
+++ pick.py
@@ -85,8 +85,7 @@ def main_curses(stdscr, lines, d):
     curses.curs_set(0)
     curses.init_pair(1, 251, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_YELLOW)
-    # change color
-    curses.init_pair(3, 238, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(4, 237, curses.COLOR_BLACK)
 
     offset = (0, 0)
